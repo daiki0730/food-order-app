c3781e5d98c808c95f044121d7618c43cbf009c0
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-13 21:21:38 +0900

    引数をセット
    個数を表示させるため

--- data.php
+++ data.php
@@ -4,6 +4,7 @@
 require_once('menu.php');
 
 $juice = new Menu('JUICE',600,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
+$juice -> setOrderCount(3);
 $coffee = new Menu('COFFEE',500,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
 $curry = new Menu('CURRY',900,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
 $pasta = new Menu('PASTA',1200,'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
