89d9400449e7b6a27d2b55489c71bcf624b05257
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-13 17:13:49 +0900

    コンストラクタを使用
    __constructを定義

--- index.php
+++ index.php
@@ -2,13 +2,20 @@
 
 class Menu {
   public $name;
+
+  public function __construct() {
+    echo '注文が入りました';
+  }
+
   public function hello(){
     echo '注文は'.$this->name.'です';
   }
 }
 
 $curry = new Menu();
+echo '<br>';
 $pasta = new Menu();
+echo '<br>';
 $curry->name = 'CURRY';
 $pasta->name = 'PASTA';
 
