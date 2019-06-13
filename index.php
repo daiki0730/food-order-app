fac6ad50dc3375fd80d60203f5e1ca001fb2caab
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-13 16:42:39 +0900

    Menuクラスにプロパティを設定
    nameを設定してインスタンスに代入した

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
 class Menu {
+  public $name;
 }
 
 $curry = new Menu();
-$pasta = new Menu();
+$curry->name = 'CURRY';
 
+$pasta = new Menu();
+$pasta->name = 'PASTA';
+
+echo $curry->name;
+echo '<br>';
+echo $pasta->name;
 
 ?>
