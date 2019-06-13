fc4c5e66ea192d8dc795019052fc6174775f0418
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-13 19:14:43 +0900

    html/stylesheetを定義

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
 <?php
-
 class Menu {
   public $name;
-
+  
   public function __construct($name) {
     $this->name = $name;
   }
-
-  public function hello(){
-    echo '注文は'.$this->name.'です';
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
   }
 }
 
+$juice = new Menu('JUICE');
+$coffee = new Menu('COFFEE');
 $curry = new Menu('CURRY');
-echo '<br>';
 $pasta = new Menu('PASTA');
-echo '<br>';
+
 
 ?>
 
-<p><?php echo $curry->name; ?></p>
-<p><?php echo $pasta->name; ?></p>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Nishida Daiki</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Nishida Daiki</h1>
+    <div class="menu-items">
+
+      
+      
+    </div>
+  </div>
+</body>
+</html>
