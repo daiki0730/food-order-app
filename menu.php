bf844ed7dd21ce4f51f29fea80171aa8f3322db8
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-13 19:50:04 +0900

    ファイルを目的別に分ける
    index表示用
    dataクラス定義用
    menuデータ定義用

---
+++ menu.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('data.php');
+
+$juice = new Menu('JUICE');
+$coffee = new Menu('COFFEE');
+$curry = new Menu('CURRY');
+$pasta = new Menu('PASTA');
+
+$menues = array($juice, $coffee, $curry, $pasta);
+
+?>
