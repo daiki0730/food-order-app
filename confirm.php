ec2914642a4a7eeccbc18c89b02c4bdcaa175529
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-14 23:45:00 +0900

    confirmファイルを作成
    送信された値を受け取るため

---
+++ confirm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="order-wrapper">
+    <h2>注文内容確認</h2>
+    <?php foreach ($menus as $menu): ?>
+      <?php ?>
+      <p class="order-amount">
+
+        
+        個
+      </p>
+    <?php endforeach ?>
+  </div>
+</body>
+</html>
