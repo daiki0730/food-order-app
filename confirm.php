09a1bb9f3ef7baeb62a697a26af3d7af8ece7ba6
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-17 16:43:58 +0900

    合計金額の表示

--- confirm.php
+++ confirm.php
@@ -1,4 +1,3 @@
-
 <?php require_once('data.php') ?>
 
 <!DOCTYPE html>
@@ -12,15 +11,24 @@
 <body>
   <div class="order-wrapper">
     <h2>注文内容確認</h2>
+    <?php $totalPayment = 0 ?>
+    
     <?php foreach ($menus as $menu): ?>
-      <?php $orderCount = $_POST[$menu->getName()] ?>
+      <?php 
+        $orderCount = $_POST[$menu->getName()];
+        $menu->setOrderCount($orderCount);
+        $totalPayment += $menu->getTotalPrice();
+        
+      ?>
       <p class="order-amount">
         <?php echo $menu->getName() ?>
         x
         <?php echo $orderCount ?>
         個
       </p>
+      <p class="order-price"><?php echo $menu->getTotalPrice() ?>円</p>
     <?php endforeach ?>
+    <h3>合計金額: <?php echo $totalPayment ?>円</h3>
   </div>
 </body>
 </html>
